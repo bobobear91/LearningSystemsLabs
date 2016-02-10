f79ede9a7cb8545e00c1beb0ff26befd9ec1b1ba
bobo91 <r.calmegard@gmail.com>
2016-02-10 10:50:29 +0100

    Added project 2

---
+++ Lab 2/Lab assignment 2/Lab assignment 2/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Lab_assignment_2.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_assignment_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Variables
+        private MainViewModel viewModel;
+
+        #endregion
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            //***********************************************************************
+            //      MainView Model
+            //***********************************************************************
+            this.viewModel = new MainViewModel();
+            this.DataContext = viewModel;
+
+        }
+    }
+}
---
+++ Lab 2/Lab assignment 2/Lab assignment 2/ViewModel/MainViewModel.cs
@@ -0,0 +1,86 @@
+﻿using Lab_assignment_2.Handlers;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace Lab_assignment_2.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        #region View-Events
+        public ICommand StartSimulation { get; private set; }
+        public ICommand StopSimulation { get; private set; }
+        #endregion
+
+
+        #region Constructor
+        public MainViewModel()
+        {
+
+            OutputText = new ObservableCollection<string>();
+
+            StartSimulation = new RelayCommand<object>(StartSimulationEvent);
+        }
+        #endregion
+
+        #region Events
+        public void StartSimulationEvent(object parameter)
+        {
+            OutputText.Add("Some dummy string");
+            
+        }
+        #endregion
+
+        #region Properties
+        public ObservableCollection<string> OutputText { get; set; }
+
+        private bool isRunning = false;
+        public bool IsRunning
+        {
+            get { return isRunning; }
+            private set
+            {
+                if (isRunning != value)
+                {
+                    isRunning = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private bool isEnabled = true;
+        public bool IsEnabled
+        {
+            get { return isEnabled; }
+            set
+            {
+                if (isEnabled != value)
+                {
+                    isEnabled = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
