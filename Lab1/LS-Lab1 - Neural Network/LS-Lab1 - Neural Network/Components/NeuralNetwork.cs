10685c69453a2ff96a113125297e3084d512ba1d
Dennis Stockhaus <destarfighter@gmail.com>
2016-02-11 15:15:13 +0100

    Update - NeuralNetwork
    Added function to normalize data

--- Lab1/LS-Lab1 - Neural Network/LS-Lab1 - Neural Network/Components/NeuralNetwork.cs
+++ Lab1/LS-Lab1 - Neural Network/LS-Lab1 - Neural Network/Components/NeuralNetwork.cs
@@ -199,6 +199,43 @@ namespace LS_Lab1___Neural_Network.Components
             }
             Weights = initialWeights;
         }
+        private double[][] NormilizeData(double[][] data)
+        {
+            // Compute the max and min value for each column in 'data'.
+            double[] maxValues = new double[data[0].Length];
+            double[] minValues = new double[data[0].Length];
+
+            for (int x = 0; x < data[0].Length; x++)
+            {
+                double maxValue = double.MinValue;
+                double minValue = double.MaxValue;
+                for (int y = 0; y < data.Length; y++)
+                {
+                    if (data[y][x] > maxValue) maxValue = data[y][x];
+                    if (data[y][x] < minValue) minValue = data[y][x];
+                }
+
+                maxValues[x] = maxValue;
+                minValues[x] = minValue;
+            }
+
+            // Compute the normalized values for all data.
+            double[][] normalizedData = new double[data.Length][];
+
+            for (int y = 0; y < data.Length; y++)
+            {
+                // Initialize inner arrays on the fly. 
+                normalizedData[y] = new double[data[0].Length];
+
+                for (int x = 0; x < data[0].Length; x++)
+                {
+                    double a = data[y][x] - minValues[x];
+                    double b = maxValues[x] - minValues[x];
+                    normalizedData[y][x] = a / b;
+                }
+            }
+            return normalizedData;
+        }
 
         private double[] ComputeOutput(double[] TrainingDataInput)
         {
@@ -299,6 +336,8 @@ namespace LS_Lab1___Neural_Network.Components
             int iterator = 0;
             double accuracy = 0;
             double error = 0;
+
+            trainingData = NormilizeData(trainingData);
 
             // Initialize container arrays, and split training Data -> input/output
             double[][] trainingInput = new double[trainingData.Length][];
